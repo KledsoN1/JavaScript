fe2c2260965f5f160ee35a0dd7ce83ebd1eb031d
KledsoN1 <kledsongame@gmail.com>
2024-11-05 20:35:57 -0300

    ex

---
+++ aula11/javascript.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60){
+    console.log('Sua velocidade esta acima da permitida, MULTADO!')
+}
+
+console.log('Dirija sempre com cinto de segurança')
---
+++ aula11/javascript2.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país} você é: `)
+if(país == 'Brasil'){
+    console.log('Brasileiro')
+
+}
+else{
+    console.log('Estrangeiro')
+}
